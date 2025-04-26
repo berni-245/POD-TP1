5cf1d025b0b6aba5df213ffc75124d9988c66c82
berni <bernizapico@gmail.com>
2025-04-25 23:41:02 -0300

    feat: added basic not synchronous Station class for saving our state

--- server/src/main/java/ar/edu/itba/pod/server/model/Station.java
+++ server/src/main/java/ar/edu/itba/pod/server/model/Station.java
@@ -1,5 +1,27 @@
 package ar.edu.itba.pod.server.model;
 
-public class Station {
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class Station {
+    ConcurrentLinkedQueue<Train> waitingTrains = new ConcurrentLinkedQueue<>();
+    ConcurrentHashMap<Integer, Platform> platforms = new ConcurrentHashMap<>();
+
+    public Platform addPlatform(Size platformSize) {
+        Platform platform = new Platform(platformSize);
+        platforms.put(platform.getId(), platform);
+        System.out.println(platforms);
+        return platform;
+    }
+
+    public Platform getPlatform(int id) {
+        return platforms.get(id);
+    }
+
+    public Platform togglePlatform(int id) {
+        Platform platform = platforms.get(id);
+        platform.togglePlatform();
+        return platform;
+    }
 }
