ce7a75204125e1b461431b55867621c7538767b2
tscheffer1 <tscheffer@itba.edu.ar>
2025-04-27 18:44:09 -0300

    Chore: added Clients Scripts

---
+++ client/src/main/assembly/overlay/run-train-client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MAIN_CLASS="ar.edu.itba.pod.client.management.TrainClient"
+java "$JAVA_OPTS" -cp 'lib/jars/*' $MAIN_CLASS "$@"
+
+# tar -xf "client/target/tpe1-g3-client-1.0-SNAPSHOT-bin.tar.gz" -C "client/target/"
+# java -cp "client/target/tpe1-g3-client-1.0-SNAPSHOT/lib/jars/*" "ar.edu.itba.pod.client.management.TrainClient" "$@"
