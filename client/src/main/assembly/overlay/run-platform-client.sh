32999d7fec44956e59c01b5b4c3e60776f893a60
tscheffer1 <tscheffer@itba.edu.ar>
2025-04-28 20:46:26 -0300

    Fix: arguments weren't getting to the properties

--- client/src/main/assembly/overlay/run-platform-client.sh
+++ client/src/main/assembly/overlay/run-platform-client.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
+PATH_TO_CODE_BASE=`pwd`
 MAIN_CLASS="ar.edu.itba.pod.client.management.PlatformClient"
-java "$JAVA_OPTS" -cp 'lib/jars/*' $MAIN_CLASS "$@"
-
-# tar -xf "client/target/tpe1-g3-client-1.0-SNAPSHOT-bin.tar.gz" -C "client/target/"
-# java -cp "client/target/tpe1-g3-client-1.0-SNAPSHOT/lib/jars/*" "ar.edu.itba.pod.client.management.PlatformClient" "$@"
+java "$@" -cp 'lib/jars/*' $MAIN_CLASS
--- client/src/main/assembly/overlay/run-report-client.sh
+++ client/src/main/assembly/overlay/run-report-client.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 MAIN_CLASS="ar.edu.itba.pod.client.operations.ReportClient"
-java "$JAVA_OPTS" -cp 'lib/jars/*' $MAIN_CLASS "$@"
-
-# tar -xf "client/target/tpe1-g3-client-1.0-SNAPSHOT-bin.tar.gz" -C "client/target/"
-# java -cp "client/target/tpe1-g3-client-1.0-SNAPSHOT/lib/jars/*" "ar.edu.itba.pod.client.operations.ReportClient" "$@"
+java "$@" -cp 'lib/jars/*' $MAIN_CLASS
