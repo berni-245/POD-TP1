d3734eefe3a71d634a87bfecc968150aaaa92d2e
berni-245 <bernizapico@gmail.com>
2025-05-03 19:08:50 -0300

    fix: fixed a weird bug where all .sh were in a wrong CRLF line terminator

--- client/src/main/assembly/overlay/train.sh
+++ client/src/main/assembly/overlay/train.sh
@@ -2,3 +2,4 @@
 
 MAIN_CLASS="ar.edu.itba.pod.client.TrainClient"
 java "$@" -cp 'lib/jars/*' $MAIN_CLASS
+
--- server/src/main/assembly/overlay/run-server.sh
+++ server/src/main/assembly/overlay/run-server.sh
@@ -5,6 +5,4 @@ PATH_TO_CODE_BASE=`pwd`
 #JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"	
 
 MAIN_CLASS="ar.edu.itba.pod.server.Server"
-
-
 java "$@" -cp 'lib/jars/*' $MAIN_CLASS
